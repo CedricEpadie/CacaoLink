4643d906c0195bdc3cf43ef2731ff126905ea198
CedricEpadie <oceane.epadie@gmail.com>
2025-01-30 13:25:43 +0100

    Configuration du CustomUser | Installation des modules django_filters pour le filtrage avancé des api (Faciliter le front), ainsi que drf_yasg pour la documentation avec swagger

--- cacaolink/urls.py
+++ cacaolink/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Api pour la documentation avec swagger
+schema_view = get_schema_view(
+    openapi.Info(title="API Documentation", default_version="v1"),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cacaolinkapp.urls'))
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-ui"), # API swagger pour la documentation de l'api
+    # path('', include('cacaolinkapp.urls'))
 ]
