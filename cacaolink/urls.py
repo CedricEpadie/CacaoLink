91db1f1f6ad500337e18254af7201d7f207d3a17
CedricEpadie <oceane.epadie@gmail.com>
2025-01-31 11:22:22 +0100

    Suppresion de l'url de cacaolinkapp

--- cacaolink/urls.py
+++ cacaolink/urls.py
@@ -12,6 +12,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-ui"), # API swagger pour la documentation de l'api
-    # path('', include('cacaolinkapp.urls')),
     path('auth_app/', include('auth_app.urls'))
 ]
