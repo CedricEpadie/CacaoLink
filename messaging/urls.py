51820fe3e384ee5e7eb37f16115ca76ed27216af
CedricEpadie <oceane.epadie@gmail.com>
2025-02-06 11:52:53 +0100

    Modification de l'url de send messaging

--- messaging/urls.py
+++ messaging/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import send_message, get_messages, mark_message_as_read
 
 urlpatterns = [
-    path('send/', send_message, name='send_message'),
+    path('send/<int:user_id>/', send_message, name='send_message'),
     path('messages/<int:user_id>/', get_messages, name='get_messages'),
     path('messages/read/<int:message_id>/', mark_message_as_read, name='mark_message_as_read'),
 ]
