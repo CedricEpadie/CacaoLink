c8262a5fbf55b1f8b3ab65903899e8dbd3546097
CedricEpadie <oceane.epadie@gmail.com>
2025-02-06 11:47:57 +0100

    Modification de la fonction de l'envoie de message

--- messaging/views.py
+++ messaging/views.py
@@ -14,10 +14,16 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 @api_view(['POST'])
-def send_message(request):
+def send_message(request, user_id):
+    try:
+        receiver = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
     serializer = MessageSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
+        # Assign the receiver to the message
+        serializer.save(receiver=receiver, sender=User)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
